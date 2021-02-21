0657bcbfaaaf9c025dbc6c637f5a4a7142b82ab5
Douglas Gomes Modesto <d.modesto@guide.com.br>
2021-02-21 11:47:39 -0300

    feat(movie-list): criar o template da página 'Lista de Filmes' (MovieListComponent)

--- src/app/pages/movie-list/movie-list.component.ts
+++ src/app/pages/movie-list/movie-list.component.ts
@@ -11,16 +11,16 @@ export class MovieListComponent implements OnInit {
   constructor() { }
 
   movies = [
-    { id: 1, title: 'Bastardos Inglórios', obs: 'Dolore eiusmod duis sunt irure occaecat sit reprehenderit sunt aute laborum eu ipsum amet.', rating: 5, toWatchNext: true },
-    { id: 2, title: 'Guardiões da Galáxia', obs: 'Dolore eiusmod duis sunt irure occaecat sit reprehenderit sunt aute laborum eu ipsum amet.', rating: 5, toWatchNext: true },
-    { id: 3, title: 'Um Contratempo', obs: 'Dolore eiusmod duis sunt irure occaecat sit reprehenderit sunt aute laborum eu ipsum amet.', rating: 5, toWatchNext: true },
-    { id: 4, title: 'Cidade de Deus', obs: 'Dolore eiusmod duis sunt irure occaecat sit reprehenderit sunt aute laborum eu ipsum amet.', rating: 5, toWatchNext: true },
+    { id: 1, title: 'Bastardos Inglórios', obs: 'Dolore eiusmod duis sunt irure occaecat sit reprehenderit sunt aute laborum eu ipsum amet.', rating: 3, toWatchNext: true },
+    { id: 2, title: 'Guardiões da Galáxia', obs: 'Dolore eiusmod duis sunt irure occaecat sit reprehenderit sunt aute laborum eu ipsum amet.', rating: 4.5, toWatchNext: false },
+    { id: 3, title: 'Um Contratempo', obs: 'Dolore eiusmod duis sunt irure occaecat sit reprehenderit sunt aute laborum eu ipsum amet.', rating: 4, toWatchNext: true },
+    { id: 4, title: 'Cidade de Deus', obs: 'Dolore eiusmod duis sunt irure occaecat sit reprehenderit sunt aute laborum eu ipsum amet.', rating: 4, toWatchNext: true },
     { id: 5, title: 'Corra', obs: 'Dolore eiusmod duis sunt irure occaecat sit reprehenderit sunt aute laborum eu ipsum amet.', rating: 5, toWatchNext: true },
-    { id: 6, title: 'Django', obs: 'Dolore eiusmod duis sunt irure occaecat sit reprehenderit sunt aute laborum eu ipsum amet.', rating: 5, toWatchNext: true },
-    { id: 7, title: 'O Resgate do Soldado Ryan', obs: 'Dolore eiusmod duis sunt irure occaecat sit reprehenderit sunt aute laborum eu ipsum amet.', rating: 5, toWatchNext: true },
-    { id: 8, title: 'Pulp Fiction', obs: 'Dolore eiusmod duis sunt irure occaecat sit reprehenderit sunt aute laborum eu ipsum amet.', rating: 5, toWatchNext: true },
+    { id: 6, title: 'Django', obs: 'Dolore eiusmod duis sunt irure occaecat sit reprehenderit sunt aute laborum eu ipsum amet.', rating: 5, toWatchNext: false },
+    { id: 7, title: 'O Resgate do Soldado Ryan', obs: 'Dolore eiusmod duis sunt irure occaecat sit reprehenderit sunt aute laborum eu ipsum amet.', rating: 5, toWatchNext: false },
+    { id: 8, title: 'Pulp Fiction', obs: 'Dolore eiusmod duis sunt irure occaecat sit reprehenderit sunt aute laborum eu ipsum amet.', rating: 5, toWatchNext: false },
     { id: 9, title: 'O irlândes', obs: 'Dolore eiusmod duis sunt irure occaecat sit reprehenderit sunt aute laborum eu ipsum amet.', rating: 5, toWatchNext: true },
-    { id: 10, title: 'Os suspeitos', obs: 'Dolore eiusmod duis sunt irure occaecat sit reprehenderit sunt aute laborum eu ipsum amet.', rating: 5, toWatchNext: true },
+    { id: 10, title: 'Os suspeitos', obs: 'Dolore eiusmod duis sunt irure occaecat sit reprehenderit sunt aute laborum eu ipsum amet.', rating: 5, toWatchNext: false },
     { id: 11, title: 'John Wick', obs: 'Dolore eiusmod duis sunt irure occaecat sit reprehenderit sunt aute laborum eu ipsum amet.', rating: 5, toWatchNext: true },
     { id: 12, title: 'Parasita', obs: 'Dolore eiusmod duis sunt irure occaecat sit reprehenderit sunt aute laborum eu ipsum amet.', rating: 5, toWatchNext: true },
   ];
