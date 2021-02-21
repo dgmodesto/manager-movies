fe62f64a1a6e678cd6928f23ebed480ccca9f6c6
Douglas Gomes Modesto <d.modesto@guide.com.br>
2021-02-21 11:00:40 -0300

    feat(about): criar página about component

--- src/app/app.module.tns.ts
+++ src/app/app.module.tns.ts
@@ -2,6 +2,7 @@ import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { NativeScriptModule } from '@nativescript/angular';
 import { AppRoutingModule } from '@src/app/app-routing.module';
 import { AppComponent } from '@src/app/app.component';
+import { AboutComponent } from '@src/app/pages/about/about.component';
 
 
 
@@ -14,6 +15,7 @@ import { AppComponent } from '@src/app/app.component';
 @NgModule({
   declarations: [
     AppComponent,
+    AboutComponent,
   ],
   imports: [
     NativeScriptModule,
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,14 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from '@src/app/pages/about/about.component';
 
 
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: '/home',
+    redirectTo: '/about',
     pathMatch: 'full',
+  }, {
+    path: 'about',
+    component: AboutComponent
   }
 ];
